92a48c22c6fe1ea7f2d59c5469b80d86be58e87d
liavaldetaro <medfys-shared@MFP520c01.mfdm.rm.dk>
2020-10-04 18:15:11 +0200

    l

--- experiments_02_10_2020/run_topas.sh
+++ experiments_02_10_2020/run_topas.sh
@@ -1,12 +1,25 @@
 echo "writing the topas files ..."
-python3 generate_topas_files.py 75 2000 10000000 1
-python3 generate_topas_files.py 95 2500 10000000 0
-python3 generate_topas_files.py 120 2500 10000000 3
+#python3 generate_topas_files.py 75 2000 10000000 1
+#python3 generate_topas_files.py 95 2500 10000000 0
+#python3 generate_topas_files.py 120 2500 10000000 3
+~/topas/bin/topas beam75.txt
 git add --all
-git commit -m 'simulations'
+git commit -m '75 mev'
 git push
-liavaldetaro
-Rmit2901l4r8z9
-#~/topas/bin/topas beam75.txt
-#~/topas/bin/topas beam95.txt
-#~/topas/bin/topas beam120.txt
+~/topas/bin/topas beam95.txt
+git add --all
+git commit -m '95 mev'
+git push
+~/topas/bin/topas beam120.txt
+git add --all
+git commit -m '120 mev'
+git push
+~/topas/bin/topas sobp_field.txt
+git add --all
+git commit -m 'sobp'
+git push
+~/topas/bin/topas vol3_field.txt
+git add --all
+git commit -m 'vol3'
+git push
+
