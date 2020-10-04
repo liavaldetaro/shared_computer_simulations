a0de2fee97a65024db76f1f48686f09711343041
liavaldetaro <medfys-shared@MFP520c01.mfdm.rm.dk>
2020-10-04 18:10:49 +0200

    new

--- experiments_02_10_2020/run_topas.sh
+++ experiments_02_10_2020/run_topas.sh
@@ -2,6 +2,11 @@ echo "writing the topas files ..."
 python3 generate_topas_files.py 75 2000 10000000 1
 python3 generate_topas_files.py 95 2500 10000000 0
 python3 generate_topas_files.py 120 2500 10000000 3
+git add --all
+git commit -m 'simulations'
+git push
+liavaldetaro
+Rmit2901l4r8z9
 #~/topas/bin/topas beam75.txt
 #~/topas/bin/topas beam95.txt
 #~/topas/bin/topas beam120.txt
