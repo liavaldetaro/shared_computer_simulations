a6bb2057de71c025fbaed317204e8d61675efbb3
liavaldetaro <liavaldetaro@gmail.com>
2020-10-06 18:47:58 +0200

    fixed beam files

--- experiments_02_10_2020/run_topas.sh
+++ experiments_02_10_2020/run_topas.sh
@@ -1,7 +1,7 @@
 echo "writing the topas files ..."
-#python3 generate_topas_files.py 75 2000 10000000 1
-#python3 generate_topas_files.py 95 2500 10000000 0
-#python3 generate_topas_files.py 120 2500 10000000 3
+python3 generate_topas_files.py 75 2000 5000000 10
+python3 generate_topas_files.py 95 2500 5000000 0
+python3 generate_topas_files.py 120 2500 5000000 30
 #~/topas/bin/topas beam75.txt
 #git add --all
 #git commit -m '75 mev'
@@ -14,12 +14,12 @@ echo "writing the topas files ..."
 #git add --all
 #git commit -m '120 mev'
 #git push
-~/topas/bin/topas sobp_field.txt
-git add --all
-git commit -m 'sobp'
-git push
-~/topas/bin/topas vol3_field.txt
-git add --all
-git commit -m 'vol3'
-git push
+#~/topas/bin/topas sobp_field.txt
+#git add --all
+#git commit -m 'sobp'
+#git push
+#~/topas/bin/topas vol3_field.txt
+#git add --all
+#git commit -m 'vol3'
+#git push
 
